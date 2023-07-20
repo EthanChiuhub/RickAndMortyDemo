9b2ac3b96990abea5062db2e01f826b65323e8f3
Ethan <shin20463@gmail.com>
2023-07-20 18:05:12 +0800

    Added some models

---
+++ RickyAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickyAndMorty
+//
+//  Created by Yi Chun Chiu on 2023/7/20.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case unknown = "unknown"
+}
---
+++ RickyAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickyAndMorty
+//
+//  Created by Yi Chun Chiu on 2023/7/20.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
