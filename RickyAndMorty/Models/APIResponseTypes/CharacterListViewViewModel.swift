219836347e0358fe1c290d1c2c0cd98569f642dc
EthanChiu <shin20463@gmail.com>
2023-07-31 22:12:56 +0800

    In CharacterListView added collectionView and ViewModel and collectionViewDelegate in CharacterListViewModel

---
+++ RickyAndMorty/Models/APIResponseTypes/CharacterListViewViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  CharacterListViewViewModel.swift
+//  RickyAndMorty
+//
+//  Created by Yi Chun Chiu on 2023/7/31.
+//
+
+import UIKit
+
+final class CharacterListViewViewModel: NSObject {
+    func fetchCharacters() {
+        RMService.shared.execute(.listCharacterRequests, expecting: RMGetAllCharacterResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print(String(describing: model))
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        return CGSize(width: width, height: width * 1.5)
+    }
+    
+}
