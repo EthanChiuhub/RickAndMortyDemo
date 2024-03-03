4e9be66feb16002940fade409612b03b52596a4c
EthanChiu <shin20463@gmail.com>
2024-03-03 21:35:34 +0800

    - [Update] Modify combine

--- RickyAndMorty/APIClient/RMService.swift
+++ RickyAndMorty/APIClient/RMService.swift
@@ -31,49 +31,34 @@ final class RMService {
     public func execute<T: Codable>(
         _ request: RMRequest,
         expecting type: T.Type
-    ) -> Future<T, Error> {
+    ) -> AnyPublisher<T, AFError> {
         if let cachedData = cacheManager.cachedResponse(
             for: request.endpoint,
             url: request.url
         ) {
-            do {
-                let result = try JSONDecoder().decode(type.self, from: cachedData)
-                print("Using cached API Response")
-                return Future { promise in
-                    promise(.success(result))
-                }
-            } catch  {
-                return Future { promise in
-                    promise(.failure(error))
-                }
-            }
+//            do {
+//                let result = try JSONDecoder().decode(type.self, from: cachedData)
+//                print("Using cached API Response")
+//                return Future { promise in
+//                    promise(.success(result))
+//                }
+//            } catch  {
+//                return Future { promise in
+//                    promise(.failure(error))
+//                }
+//            }
         }
         guard let urlRequest = self.request(from: request) else {
-            return Future { promise in
-                promise(.failure(RMServiceError.failedToCreateRequest))
-            }
+            return Fail(error: RMServiceError.failedToCreateRequest as! AFError).eraseToAnyPublisher()
         }
-        return Future<T, Error> { promise in
-               AF.request(urlRequest)
-                   .publishDecodable(type: type)
-                   .value()
-                   .sink(
-                       receiveCompletion: { completion in
-                           switch completion {
-                           case .finished:
-                               break // Do nothing for success
-                           case .failure(let error):
-                               promise(.failure(error))
-                           }
-                       },
-                       receiveValue: { value in
-                           promise(.success(value))
-                           // If needed, you can also set the cache here
-//                           self.cacheManager.setCache(for: request.endpoint, url: request.url, data: value)
-                       }
-                   )
-                   .store(in: &self.cancellables)
-           }
+        
+        return AF
+            .request(urlRequest)
+            .publishDecodable(type: type)
+            .value().mapError { error in
+                return AFError.requestAdaptationFailed(error: error)
+            }.eraseToAnyPublisher()
+
     }
     
     // MARK: - Private
